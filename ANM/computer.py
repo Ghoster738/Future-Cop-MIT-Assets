420557c87df151802a5ab6c5d08c81c5f4db14df
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2023-04-27 12:48:08 -0700

    ANM resource under CC0

---
+++ ANM/computer.py
@@ -0,0 +1,4 @@
+import ANMBuilder
+
+ANMBuilder.writeANMFile( "Frames", "Frames/0003.png", "win.canm", ANMBuilder.Platform.Windows )
+ANMBuilder.writeANMFile( "Frames", "Frames/0003.png", "mac.canm", ANMBuilder.Platform.Macintosh )
