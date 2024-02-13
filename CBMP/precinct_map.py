f671f650bb25d87d92625b76e8c814df8d2699c0
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-02-12 16:46:40 -0800

    I made a custom precinct assualt texture.
    I think the texture needs some more work, but it is good enough for now.
    
    This CBMP file is designed with every visual feature of CTIL, the map
    chunk format, in mind.
    It has areas where frame by frame animations can be done.
    It has areas where displacement animations can be played.
    The CBMP texture even has lava and water areas.
    
    The playstation CBMP seems to have some kind of problem though. I will
    fix this eventually.

---
+++ CBMP/precinct_map.py
@@ -0,0 +1,5 @@
+import CBMPBuilder
+
+CBMPBuilder.writeCBMPFilePath( "precinct_map.png", "precinct_map_windows.cbmp", CBMPBuilder.Platform.Windows )
+CBMPBuilder.writeCBMPFilePath( "precinct_map.png", "precinct_map_macintosh.cbmp", CBMPBuilder.Platform.Macintosh )
+CBMPBuilder.writeCBMPFilePath( "precinct_map.png", "precinct_map_playstation.cbmp", CBMPBuilder.Platform.Playstation )
