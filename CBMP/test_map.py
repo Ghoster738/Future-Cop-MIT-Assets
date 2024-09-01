16d7b3a4436bfba1770fea48ef8d3791223f3ae4
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2024-09-01 13:12:30 -0700

    TEST_MAP Cbmp is there to test sections

---
+++ CBMP/test_map.py
@@ -0,0 +1,5 @@
+import CBMPBuilder
+
+CBMPBuilder.writeCBMPFilePath( "test_map.webp", "windows_test_map.cbmp", CBMPBuilder.Platform.Windows )
+CBMPBuilder.writeCBMPFilePath( "test_map.webp", "macintosh_test_map.cbmp", CBMPBuilder.Platform.Macintosh )
+CBMPBuilder.writeCBMPFilePath( "test_map.webp", "playstation_test_map.cbmp", CBMPBuilder.Platform.Playstation )
