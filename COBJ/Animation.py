2edfb1740b06905b8554c821fb16cce079e10606
Ghoster738 <101094904+Ghoster738@users.noreply.github.com>
2025-04-30 01:09:42 -0700

    Digit Amount can be specified

--- COBJ/Animation.py
+++ COBJ/Animation.py
@@ -1,7 +1,17 @@
 import COBJBuilder
 import zlib
 
-def generateModel():
+def generateModel( digit_amount : int, width : int = 256):
+    color_difference = int(255 / digit_amount)
+
+    base_digit_width = int((2 * width) / digit_amount)
+    base_digit_position = -width + int(base_digit_width / 2)
+
+    base_triangle_vertex_amount  = 3
+    root_triangles_vertex_amount = digit_amount + 1
+    top_triangles_vertex_amount  = digit_amount
+    number_of_vertices = base_triangle_vertex_amount + root_triangles_vertex_amount + top_triangles_vertex_amount
+
     model = COBJBuilder.Model()
 
     model.setEnvironmentMapSemiTransparent(False)
@@ -11,9 +21,9 @@ def generateModel():
     testFaceTypes[-1].setVertexColor(True, [0, 0xff, 0])
     model.appendFaceType(testFaceTypes[-1])
 
-    for i in range(0, 8):
+    for i in range(0, digit_amount):
         testFaceTypes.append( COBJBuilder.FaceType() )
-        testFaceTypes[-1].setVertexColor(True, [31 * (8 - i), 0, 31 * i])
+        testFaceTypes[-1].setVertexColor(True, [color_difference * (digit_amount - i), 0, color_difference * i])
         model.appendFaceType(testFaceTypes[-1])
 
     face = COBJBuilder.Primitive()
@@ -30,37 +40,32 @@ def generateModel():
     face.setFaceTypeIndex(0)
     model.appendPrimitive(face)
 
-    for i in range(0, 8):
+    for i in range(0, digit_amount):
         face = COBJBuilder.Primitive()
-        face.setTypeTriangle([3 + i, 12 + i, 4 + i], [0, 0, 0])
+        face.setTypeTriangle([3 + i, 4 + digit_amount + i, 4 + i], [0, 0, 0])
         face.setTexture(False)
         face.setReflective(False)
         face.setFaceTypeIndex(1 + i)
         model.appendPrimitive(face)
 
         face = COBJBuilder.Primitive()
-        face.setTypeTriangle([4 + i, 12 + i, 3 + i], [0, 0, 0])
+        face.setTypeTriangle([4 + i, 4 + digit_amount + i, 3 + i], [0, 0, 0])
         face.setTexture(False)
         face.setReflective(False)
         face.setFaceTypeIndex(1 + i)
         model.appendPrimitive(face)
-
-    base_triangle_vertex_amount = 3
-    root_triangles_vertex_amount = 9
-    top_triangles_vertex_amount = 8
-    number_of_vertices = base_triangle_vertex_amount + root_triangles_vertex_amount + top_triangles_vertex_amount
 
     model.allocateVertexBuffers(256, number_of_vertices, 1, 0, 0, 0)
 
     for i in range(0, 256):
         positionBuffer = model.getPositionBuffer(i)
 
-        positionBuffer.setValue(0, (-256, 64, 0))
-        positionBuffer.setValue(1, ( 256,  0, 0))
-        positionBuffer.setValue(2, ( 256, 64, 0))
+        positionBuffer.setValue(0, (-width, 64, 0))
+        positionBuffer.setValue(1, ( width,  0, 0))
+        positionBuffer.setValue(2, ( width, 64, 0))
 
         for r in range(0, root_triangles_vertex_amount):
-            positionBuffer.setValue(base_triangle_vertex_amount + r, (-256 + 64 * r, 80, 0))
+            positionBuffer.setValue(base_triangle_vertex_amount + r, (-width + base_digit_width * r, 80, 0))
 
         for r in range(0, top_triangles_vertex_amount):
             level = 80
@@ -68,7 +73,7 @@ def generateModel():
             if ((1 << r) & i) != 0:
                 level = 256
 
-            positionBuffer.setValue(base_triangle_vertex_amount + root_triangles_vertex_amount + r, (-224 + 64 * r, level, 0))
+            positionBuffer.setValue(base_triangle_vertex_amount + root_triangles_vertex_amount + r, (base_digit_position + base_digit_width * r, level, 0))
 
         positionBuffer = model.getNormalBuffer(i)
         positionBuffer.setValue(0, (0, 0, 4096))
@@ -78,12 +83,12 @@ def generateModel():
     return model
 
 def generate():
-    generateModel().makeFile("animation.cobj", COBJBuilder.ModelFormat.WINDOWS)
+    generateModel(8).makeFile("animation.cobj", COBJBuilder.ModelFormat.WINDOWS)
 
 def test():
     result = True
 
-    crc32 = zlib.crc32(generateModel().makeResource(COBJBuilder.ModelFormat.WINDOWS))
+    crc32 = zlib.crc32(generateModel(8).makeResource(COBJBuilder.ModelFormat.WINDOWS))
 
     if crc32 != 0xf250b466:
         result = False
